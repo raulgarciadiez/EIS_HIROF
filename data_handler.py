383148ab44c73e46c434b166f777e0902b6781fe
raulgarciadiez <raulgarcia10@gmail.com>
2024-10-10 06:36:08 +0000

    data handler file

--- data_handler.py
+++ data_handler.py
@@ -8,14 +8,11 @@ class DataHandler:
 
     def compute_ewe(self):
         """Compute the Ewe parameter."""
-        self.ewe = self.df["<Ewe>/V"].mean()
-        return self.ewe
+        self.Ewe = self.df["<Ewe>/V"].mean()
+        return self.Ewe
 
-    def filter_frequencies(self, fmin=None, fmax=None):
+    def filter_frequencies(self, fmin=4, fmax=1e6):
         """Filter data based on a frequency range."""
         self.filtered_df = self.df.copy()
-        if fmin:
-            self.filtered_df = self.filtered_df[self.filtered_df['frequencies'] >= fmin]
-        if fmax:
-            self.filtered_df = self.filtered_df[self.filtered_df['frequencies'] <= fmax]
+        self.filtered_df = self.filtered_df[(self.filtered_df['freq/Hz'] >= fmin) & (self.filtered_df['freq/Hz'] <= fmax)& (self.filtered_df['-Im(Z)/Ohm'] > 0)]
         return self.filtered_df
